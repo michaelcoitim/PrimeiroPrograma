8b11cbc39d6e0ff969646f84ebfb4b2dcae277fe
Michael Robert Coitim <michaelcoitim1@gmail.com>
2021-01-31 23:30:30 -0200

    quebrando a maldição olá mundo

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+
+package primeiroprograma;
+
+/**
+ * @author micha
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    
+}
